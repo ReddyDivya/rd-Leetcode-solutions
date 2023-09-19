f2ac8f7af2b2326e7f0cb68775961d0743ea9d7c
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-09-19 00:45:27 -0700

    Update TwoSum.js

--- 01-Two Sum/TwoSum.js
+++ 01-Two Sum/TwoSum.js
@@ -1,17 +1,31 @@
 var twoSum = function(nums, target) {
+    /*
+    i-0 - 1st iteration
+    i-1 - 2nd iteration
+    */
     for(let i=0; i < nums.length; i++)
     {
+        /*
+        j=i+1 
+        i-0, j-[1,2]
+        i-1, j-[2]
+        */
         for(let j=i+1; j < nums.length; j++)
         {
+            /*
+            i-0, j-1 => 3+2 = 5
+            i-0, j-2 => 3+4 = 7
+            i-1, j-2 => 2+4 = 6 => got target
+            */
             if(nums[i] + nums[j] === target)
             {
-                return [i, j];
+                return [i, j]; //[1, 2]
             }
         }
     }
 };
 
 //Output
-twoSum([2,7,11,15], 9); //[0, 1]
 twoSum([3,2,4], 6); //[1, 2]
+twoSum([2,7,11,15], 9); //[0, 1]
 twoSum([3,3], 6); //[0, 1]
