32a686b3084615f8756c03e74d2a4766a1d4fe82
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-09-22 06:55:16 -0700

    Create reverse.js

---
+++ 7-Reverse Integer/reverse.js
@@ -0,0 +1,29 @@
+var reverse = function(x) {
+    let lastDigit = 0;
+    let reverse = 0;
+    let isNegNum = false;
+
+    if(x < 0)
+    {
+        console.log('negative');
+        isNegNum = true;
+    }
+
+    while(x !== 0)
+    {
+        lastDigit = x % 10;//3, 2, 1
+
+        /*
+        0*10+3 = 3,
+        3*10+2 = 32,
+        32*10+1 = 321
+        */
+        reverse = reverse*10 + lastDigit;
+        x = Math.floor(x/10);//12, 1, 0
+    }
+
+    return (isNegNum ? "-"+reverse : reverse);
+};
+
+console.log(reverse(123));//321
+console.log(reverse(-123));//-321
