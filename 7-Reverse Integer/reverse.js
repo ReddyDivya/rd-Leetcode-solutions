bdd5928431f3c729f923b5ce29c3f7d099f827a6
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-09-22 07:41:43 -0700

    Update reverse.js

--- 7-Reverse Integer/reverse.js
+++ 7-Reverse Integer/reverse.js
@@ -3,16 +3,20 @@
  * @return {number}
  */
 var reverse = function(x) {
+    //32 bit range
+    const MIN = -2147483648;
+    const MAX = 2147483647;
+
     let lastDigit = 0;
     let reverse = 0;
     let isNegNum = false;
 
+    //negative number
     if(x < 0)
     {
         isNegNum = true;
+        x = Math.abs(x);//Math.abs - to remove negative sign
     }
-
-    x = Math.abs(x);//Math.abs - to remove negative sign
 
     while(x > 0)
     {
@@ -27,9 +31,14 @@ var reverse = function(x) {
         x = Math.floor(x/10);//12, 1, 0
     }
 
+    if(reverse < MIN || reverse > MAX)
+    {
+        return 0
+    }
+
     return (isNegNum ? "-"+reverse : reverse);
 };
 
 console.log(reverse(123));//321
 console.log(reverse(-123));//-321
-console.log(reverse(1534236469));//9646324351
+console.log(reverse(1534236469));//0 because exceeding the 32-bit range
