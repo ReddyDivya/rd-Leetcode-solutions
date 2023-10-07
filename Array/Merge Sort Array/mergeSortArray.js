50b247d8baafc5bad15576d4e29da14c56c458e3
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-10-07 00:15:35 -0700

    Update mergeSortArray.js

--- Array/Merge Sort Array/mergeSortArray.js
+++ Array/Merge Sort Array/mergeSortArray.js
@@ -1,41 +1,38 @@
-//Merge Sort Array
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+function mergeSortArray(nums1, m, nums2, n) {
+    let i = m - 1; // Index in nums1 for elements from nums1
+    let j = n - 1; // Index in nums2
+    let k = m + n - 1; // Index in nums1 for the merged result
 
-/*
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-
-Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
-
-Example
-Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-Output: [1,2,2,3,5,6]
-Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
-*/
-
-
-var merge = function(nums1, m, nums2, n) {
-    let mergeArray = [];
-    
-    for(let i=0; i<nums1.length; i++)
-    {
-        if(nums1[i] !== 0)
-            mergeArray.push(nums1[i]);
+    // While there are elements in both arrays
+    while (i >= 0 && j >= 0) {
+        // Compare elements from the end of both arrays and place the larger one in nums1
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
     }
-    
-    for(let j=0; j<nums2.length; j++)
-    {
-        if(nums2[j] !== 0)
-            mergeArray.push(nums2[j]);
+
+    // If there are remaining elements in nums2, copy them to nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
     }
-    
-    //sorting
-    return mergeArray.sort(function(a,b) {
-        return a-b;
-    });
+
+    console.log(nums1)
 };
 
 console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));//[1,2,2,3,5,6]
-console.log(merge([1], 1, [], 0));//[1]
+console.log(merge([1],1, [], 0));//[1]
 console.log(merge([0], 0, [1], 1));//[1]
