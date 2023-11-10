39d715414fdbf04f75c2f05edc109a6f6f2e4c91
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-11-10 07:24:29 -0800

    Update maxProfit.js

--- 121-Best Time to Buy and Sell Stock/maxProfit.js
+++ 121-Best Time to Buy and Sell Stock/maxProfit.js
@@ -3,21 +3,28 @@
  * @return {number}
  */
 function maxProfit(prices) {
-  let maxProfit = 0;
-  let minPrice = Number.MAX_VALUE; //1.7976931348623157e+308
+  let left=0, right=1, profit=0, maxProfit=0;
 
-  for (let i = 0; i < prices.length; i++) {
-    //1. finding min price
-    if (prices[i] < minPrice) {
-      minPrice = prices[i];
-    } 
-    else if (prices[i] - minPrice > maxProfit) //2. finding max profit
-    {
-      maxProfit = prices[i] - minPrice;
-    }
+  while(left < prices.length)
+  {
+      if(prices[left] < prices[right])
+      {
+          //calculating profit
+          profit = prices[right] - prices[left];
+          maxProfit = Math.max(maxProfit, profit);
+      }
+      else
+      {
+         /*If the price at the left day is not less than the price at the right day, 
+         it means the current left day is not a good buying day. In this case, the left pointer is       moved to the right pointer.
+         */
+         left = right;//the left pointer is moved to the right pointer
+      }
+      right++;  //The right pointer is always incremented to move to the next day.
   }
 
   return maxProfit;
 }
 
-console.log(maxProfit[7,1,5,3,6,4]);//5
+maxProfit([7,1,5,3,6,4]);//5
+maxProfit([7,6,4,3,1]);//0
