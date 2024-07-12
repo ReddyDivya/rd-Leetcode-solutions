0692b30401e903092064c803b28a1b785ec48778
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2024-07-12 09:52:50 -0700

    Update and rename SingleNumberfromArray.js to SingleNumber.java

---
+++ 136-Single Number/SingleNumber.java
@@ -0,0 +1,30 @@
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        
+        // XOR all elements in the array
+        for (int num : nums) {
+            result ^= num;
+        }
+        
+        // The result will be the single number
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Solution class
+        SingleNumber solution = new SingleNumber();
+
+        // Test case 1
+        int[] nums1 = {2, 2, 1};
+        System.out.println("Output: " + solution.singleNumber(nums1)); // Output: 1
+
+        // Test case 2
+        int[] nums2 = {4, 1, 2, 1, 2};
+        System.out.println("Output: " + solution.singleNumber(nums2)); // Output: 4
+
+        // Test case 3
+        int[] nums3 = {1};
+        System.out.println("Output: " + solution.singleNumber(nums3)); // Output: 1
+    }
+}
