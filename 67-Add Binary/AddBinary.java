61b867d98316e742e0db2dc3b93f06bf07a6a726
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2024-07-23 09:23:02 -0700

    Create AddBinary.java

---
+++ 67-Add Binary/AddBinary.java
@@ -0,0 +1,30 @@
+public class AddBinary {
+    public static String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+        
+        if (carry != 0) result.append(carry);
+        
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+        System.out.println("Input: a = \"" + a + "\", b = \"" + b + "\"");
+        System.out.println("Output: \"" + addBinary(a, b) + "\"");
+
+        a = "1010";
+        b = "1011";
+        System.out.println("Input: a = \"" + a + "\", b = \"" + b + "\"");
+        System.out.println("Output: \"" + addBinary(a, b) + "\"");
+    }
+}
