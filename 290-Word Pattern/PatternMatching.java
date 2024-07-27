f331b1cc918124bdbe985c388e007f49f6912432
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2024-07-27 09:55:42 -0700

    Create PatternMatching.java

---
+++ 290-Word Pattern/PatternMatching.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PatternMatching {
+    public static boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (!charToWord.containsKey(c)) {
+                if (wordToChar.containsKey(word)) {
+                    return false;
+                }
+                charToWord.put(c, word);
+                wordToChar.put(word, c);
+            } else {
+                if (!charToWord.get(c).equals(word)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(wordPattern("abba", "dog cat cat dog")); // true
+        System.out.println(wordPattern("abba", "dog cat cat fish")); // false
+        System.out.println(wordPattern("aaaa", "dog cat cat dog")); // false
+    }
+}
